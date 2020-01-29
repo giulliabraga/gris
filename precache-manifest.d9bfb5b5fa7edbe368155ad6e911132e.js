d8eae670edc5690de1a4860b2cd9fb51e4ccab8c
willanysilva <willany_lp@hotmail.com>
2020-01-28 23:32:56 -0300

    Updates

---
+++ precache-manifest.d9bfb5b5fa7edbe368155ad6e911132e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05ff7faa11005e948a351141ba6cbfc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3cfaecf10e8740a07c8",
+    "url": "/static/css/main.ea31d224.chunk.css"
+  },
+  {
+    "revision": "52baa360179c3f0ffac2",
+    "url": "/static/js/2.9bc606c5.chunk.js"
+  },
+  {
+    "revision": "d3cfaecf10e8740a07c8",
+    "url": "/static/js/main.3636d800.chunk.js"
+  },
+  {
+    "revision": "338b08e20f062f0fd558",
+    "url": "/static/js/runtime-main.8b61a3e2.js"
+  },
+  {
+    "revision": "17be685fa7e554afba3403e4e12a7e3b",
+    "url": "/static/media/banner.17be685f.png"
+  },
+  {
+    "revision": "fee48b49306f0f6d02ce70216feab7dc",
+    "url": "/static/media/logo.fee48b49.png"
+  }
+]);
